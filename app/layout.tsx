b99111344c5b5c0c84c22ada5f0372bfc080f4c5
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-05 17:24:08 -0300

    chore: add shadcn update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Layout</h1>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/app/_components/ui/button"
+"use client"
+import { useState } from "react"
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="mb-4 text-2xl font-bold">Vila Barbearia</h1>
-      <Button>Botão de Exemplo</Button>
-    </div>
-  )
+  const [] = useState()
+  return <Button>Hello world!</Button>
 }
 
 export default Home
