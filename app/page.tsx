1a803779207115f125e8b767ce37f23cee4e5bc8
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-05 19:22:25 -0300

    Versão inicial do projeto local

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useState } from "react"
+import { Button } from "./_components/ui/button"
+
+const Home = () => {
+  const [] = useState()
+  return <Button>test</Button>
+}
+
+export default Home
