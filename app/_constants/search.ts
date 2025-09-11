3241ffedfa6a1d8b544df33546c27ca042db0bd2
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-11 13:30:35 -0300

    feat: add Bookingitem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageURL: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageURL: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageURL: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageURL: "/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageURL: "/hidratacao.svg",
+    title: "Hidratação",
+  },
+  {
+    imageURL: "/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageURL: "/relaxamento.svg",
+    title: "Relaxamento",
+  },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 //"use client"
 //import { useState } from "react"
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
-import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
-import { Badge } from "./_components/ui/badge"
-import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react"
+import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   //const [] = useState()
-  // import Header from "./_components/header"
   // Chamar banco de dados
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
@@ -41,35 +40,17 @@ const Home = async () => {
 
         {/* Busca rápida*/}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" alt="Cabelo" width={16} height={16} />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" alt="Barba" width={16} height={16} />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg"
-              alt="Acabamento"
-              width={16}
-              height={16}
-            />
-            Navalha
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <FootprintsIcon size={16} />
-            Pézinho
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <EyeIcon size={16} />
-            Sobrancelha
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageURL}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* BANNER IMAGEM */}
@@ -83,35 +64,9 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 ml-2 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/*Esquerda*/}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit bg-blue-700">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
+        <BookingItem />
 
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage
-                    src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-                    alt="Avatar"
-                  />
-                </Avatar>
-                <p className="text-sm">Vila Barbearia</p>
-              </div>
-            </div>
-            {/*Direita*/}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Setembro</p>
-              <p className="text-2xl">10</p>
-              <p className="text-sm">14:00</p>
-            </div>
-          </CardContent>
-        </Card>
-
+        {/* Lista de barbearias */}
         <h2 className="mb-3 ml-2 mt-6 text-xs font-bold uppercase text-gray-400">
           Barbearias
         </h2>
@@ -121,6 +76,7 @@ const Home = async () => {
           ))}
         </div>
 
+        {/* Lista de barbearias populares */}
         <h2 className="mb-3 ml-2 mt-6 text-xs font-bold uppercase text-gray-400">
           Populares
         </h2>
